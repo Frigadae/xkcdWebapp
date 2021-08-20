7bb1744e8e03e06e4de48a04723234715d5aaa7c
Daniel Wang <42727264+wang-yi-yao@users.noreply.github.com>
2021-08-21 09:54:46 +1200

    add comfort content such as comic title and date

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -25,10 +25,18 @@ class Body extends React.Component {
 
     async getNewComic(element) {
         element.preventDefault();
-        if (this.state.searchNum > this.state.maxNum) {
-            console.warn("Number entered exceeds max number! Request aborted!");
+        if (isNaN(this.state.searchNum)) {
+            console.warn("Request aborted! Please enter an integer!");
+            alert("Request aborted! Please enter an integer!");
             return;
         }
+
+        if (this.state.searchNum > this.state.maxNum) {
+            console.warn("Request aborted! Number entered exceeds max number!");
+            alert("Request aborted! Number entered exceeds max number!");
+            return;
+        }
+        
         console.log(`Number entered: ${this.state.searchNum}`);
         const image = await getAPI.get(`?num=${this.state.searchNum}`)
         .then(response => {
@@ -50,21 +58,26 @@ class Body extends React.Component {
         if (this.state.getImg == null) {
             return (
                 <div>
-                    <h1>Body</h1>
-                    <h3>Loading an image...</h3>
+                    <h1>Getting the latest comic...</h1>
                 </div>
             )
         } else {
             return (
                 <div>
-                    <p>Note: The latest comic number is: {this.state.maxNum}</p>
-                    <p>Submitting any number higher than the given number, zero or none will display the latest comic</p>
+                    <p>The latest comic published has the number: {this.state.maxNum}</p>
                     <form onSubmit={this.getNewComic}>
-                        <input type="text" placeholder="Enter a number" value={this.state.searchNum} onChange={this.onChange}></input>
-                        <button type="submit" onClick={this.getNewComic}>Submit</button>
+                        <label>
+                            Please enter a number : 
+                            <input type="text" placeholder="Enter a number" value={this.state.searchNum} onChange={this.onChange}></input>
+                            <button type="submit" onClick={this.getNewComic}>Submit</button>
+                        </label>
+                        <p>Hint: You can enter nothing or 0 to fetch the latest comic.</p>
                     </form>
-                    <br></br>
-                    <img src={this.state.getImg.img} alt="A XKCD comic"></img>
+                    <div>
+                        <h3>{this.state.getImg.num}: {this.state.getImg.title}</h3>
+                        <img src={this.state.getImg.img} alt={this.state.getImg.alt}></img>
+                        <p>Comic published on: {this.state.getImg.day}/{this.state.getImg.month}/{this.state.getImg.year}</p>
+                    </div>
                 </div>
             )
         }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function Footer() {
     return (
         <div>
-            <p>React.js project made by Wang Yi Yao (Preferred name Daniel Wang)</p>
-            <p>Comics are sourced from XKCD (<a href="https://xkcd.com/">xkcd.com</a>)</p>
-            <p>The XKCD API can be found <a href="https://xkcd.com/json.html">here</a></p>
+            <p>React.js project made by Wang Yi Yao (Preferred name Daniel Wang)<br></br>
+            Comics are sourced from XKCD (<a href="https://xkcd.com/">xkcd.com</a>)<br></br>
+            The XKCD API can be found <a href="https://xkcd.com/json.html">here</a></p>
         </div>
     )
 }
