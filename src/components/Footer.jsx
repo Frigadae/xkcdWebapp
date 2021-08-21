86a8c4fdfcd294acd477342ff99be4651cbaa3c5
Daniel Wang <42727264+wang-yi-yao@users.noreply.github.com>
2021-08-21 12:07:04 +1200

    add link to react.js homepage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,8 @@ export default function Footer() {
         <div id="contentFooter">
             <footer>
                 <p>This React.js project was made by Wang Yi Yao (Daniel Wang)<br></br>
-                Comics are sourced from XKCD (<a href="https://xkcd.com/">xkcd.com</a>)<br></br>
+                More information on React.js can be found <a href="https://reactjs.org/">here</a><br></br>
+                Comics are sourced from XKCD by Randall Monroe (<a href="https://xkcd.com/">xkcd.com</a>)<br></br>
                 The XKCD API can be found <a href="https://xkcd.com/json.html">here</a></p>
             </footer>
         </div>
