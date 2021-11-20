4f6fe5e90aa4cb1b540a8dbbc421fa0b9406947e
Daniel Wang <42727264+wang-yi-yao@users.noreply.github.com>
2021-11-21 08:51:50 +1300

    change equals operator to allow 0 be entered

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -39,7 +39,7 @@ class Main extends React.Component {
         }
         
         console.log(`Number entered: ${this.state.searchNum}`);
-        if (this.state.searchNum === 0 || this.state.searchNum === null) {
+        if (this.state.searchNum == 0 || this.state.searchNum === null) {
             const jsonString = await getAPI.get(`?url=https://xkcd.com/info.0.json`)
             .then(response => {
             console.log(response.data);
