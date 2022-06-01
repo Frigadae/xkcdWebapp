2f6591ed617dfe613ce96129e1e69deb2dbe805b
Daniel Wang <42727264+wang-yi-yao@users.noreply.github.com>
2022-06-01 17:03:21 +1200

    remove the old Main component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Header from "./components/Header.jsx";
-//import Main from "./components/Main.jsx";
-import MainNew from "./components/MainNew.jsx";
+import Main from "./components/Main.jsx";
 import Footer from "./components/Footer.jsx";
 
 function App() {
