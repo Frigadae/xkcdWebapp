fd2704b491f738a490c7486f668cc93da224a173
Daniel Wang <42727264+wang-yi-yao@users.noreply.github.com>
2022-06-01 17:01:18 +1200

    fix bugs in root component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
                 <p>This React.js project was made by <a href="https://github.com/wang-yi-yao">Wang Yi Yao</a> (Daniel Wang)<br/>
                 The source code of this webapp can be found in <a href="https://github.com/wang-yi-yao/xkcdWebapp">this repo</a><br/>
                 More information on React.js can be found <a href="https://reactjs.org/">here</a><br/>
-                Comics are sourced from XKCD by Randall Munroe (<a href="https://xkcd.com/">xkcd.com</a>)<br/>
+                Comics are sourced from <a href="https://xkcd.com/">xkcd.com</a>. XKCD is by Randall Munroe <br/>
                 The XKCD API can be found <a href="https://xkcd.com/json.html">here</a></p>
             </footer>
         </div>
