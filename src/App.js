db2d9bb424cc0375387f3679c71e1c400fc9ba58
Daniel Wang <42727264+wang-yi-yao@users.noreply.github.com>
2022-06-01 15:53:26 +1200

    implement fetch requests and process json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from "./components/Header.jsx";
-import Main from "./components/Main.jsx";
+//import Main from "./components/Main.jsx";
 import MainNew from "./components/MainNew.jsx";
 import Footer from "./components/Footer.jsx";
 
--- src/components/MainNew.jsx
+++ src/components/MainNew.jsx
@@ -2,28 +2,89 @@ import React, {useState, useEffect} from "react";
 import { getAPI } from "./API";
 
 function Main() {
-    const [comicData, setComicData] = useState({getImg: null, maxNum: 0, searchNum: ""});
+    const [comicData, setComicData] = useState({num: 0, title: "", getImg: null, alt: "", day:0, month:0, year:0});
+    const [maxNum, setMaxNum] = useState(0);
+    const [input, setInput] = useState("");
+    let flag = true;
 
-    
+    let onRender = FirstLoad;
+
+    useEffect(() => {
+        getAPI.get(`?url=https://xkcd.com/info.0.json`)
+        .then(response => {
+            const receivedComicData = JSON.parse(response.data);
+            console.log(receivedComicData);
+            setMaxNum(receivedComicData.num);
+            setComicData({num: receivedComicData.num, title: receivedComicData.title, img: receivedComicData.img, alt: receivedComicData.alt,
+            day: receivedComicData.day, month: receivedComicData.month, year: receivedComicData.year});
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }, []);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        if (input > 0 && input > maxNum) {
+            flag = false;
+        } else {
+            flag = true;
+            getAPI.get(`?url=https://xkcd.com/${input}/info.0.json`)
+            .then(response => {
+                const receivedComicData = JSON.parse(response.data);
+                console.log(receivedComicData);
+                setMaxNum(receivedComicData.num);
+                setComicData({num: receivedComicData.num, title: receivedComicData.title, img: receivedComicData.img, alt: receivedComicData.alt,
+                day: receivedComicData.day, month: receivedComicData.month, year: receivedComicData.year});
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        }
+    }
 
     return (
         <div id="content">
             <div id="searchDiv">
-                <p>The latest comic published has the number: {this.state.maxNum}</p>
-                <form onSubmit={this.getNewComic}>
+                <form onSubmit={(event) => {handleSubmit(event)}}>
                     <label>
                         Please enter a number : 
-                        <input type="text" placeholder="Enter a number" value={comicData.searchNum} onChange={this.onChange}></input>
-                        <button type="submit" onClick={this.getNewComic}>Submit</button>
+                        <input type="text" placeholder="Enter a number" value={input} onChange={(event) => {setInput(event.target.value)}} 
+                        pattern={"[0-9]*"} title="Only numbers can be entered!"></input>
+                        <button type="submit">Submit</button>
                     </label>
-                    <p>Tip: You can enter nothing or 0 to fetch the latest comic.</p>
                 </form>
             </div>
-            <div id="imageDiv">
-                <h3 id="contentHeader">{this.state.getImg.num}: {this.state.getImg.title}</h3>
-                <img id="contentImg" src={this.state.getImg.img} alt={this.state.getImg.alt}></img>
-                <p>Comic published on: {this.state.getImg.day}/{this.state.getImg.month}/{this.state.getImg.year}</p>
-            </div>
+            {flag && <DisplayComic comicData={comicData}/>}
+            {!flag && <Error/>}
+        </div>
+    )
+}
+
+function DisplayComic(props) {
+    return (
+        <div id="imageDiv">
+            <h3 id="contentHeader">{props.comicData.num}: {props.comicData.title}</h3>
+            <img id="contentImg" src={props.comicData.img} alt={props.comicData.alt}></img>
+            <p>Comic published on: {props.comicData.day}/{props.comicData.month}/{props.comicData.year}</p>
+        </div>
+    )
+}
+
+function FirstLoad() {
+    return (
+        <div>
+            <h2>Getting the latest comic...</h2>
+            <h4>Please wait</h4>
+        </div>
+    )
+}
+
+function Error() {
+    return (
+        <div>
+            <h2>Invalid number</h2>
+            <h4>Please try again another time. Sorry for the inconvenience</h4>
         </div>
     )
 }
