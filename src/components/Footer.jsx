d56d9226b9b869a949ad1e16ae36a0a66438f397
Daniel Wang <42727264+wang-yi-yao@users.noreply.github.com>
2022-06-01 16:56:43 +1200

    add link to repo in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,10 @@ export default function Footer() {
     return (
         <div id="contentFooter">
             <footer>
-                <p>This React.js project was made by <a href="https://github.com/wang-yi-yao">Wang Yi Yao</a> (Daniel Wang)<br></br>
-                More information on React.js can be found <a href="https://reactjs.org/">here</a><br></br>
-                Comics are sourced from XKCD by Randall Munroe (<a href="https://xkcd.com/">xkcd.com</a>)<br></br>
+                <p>This React.js project was made by <a href="https://github.com/wang-yi-yao">Wang Yi Yao</a> (Daniel Wang)<br/>
+                The source code of this webapp can be found in <a href="https://github.com/wang-yi-yao/xkcdWebapp">this repo</a><br/>
+                More information on React.js can be found <a href="https://reactjs.org/">here</a><br/>
+                Comics are sourced from XKCD by Randall Munroe (<a href="https://xkcd.com/">xkcd.com</a>)<br/>
                 The XKCD API can be found <a href="https://xkcd.com/json.html">here</a></p>
             </footer>
         </div>
