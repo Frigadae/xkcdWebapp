046516a04bbd6dc2f9650109f6d3c88d897721a4
Daniel Wang <42727264+wang-yi-yao@users.noreply.github.com>
2024-01-01 10:56:30 +1300

    change hyperlink due to user rename

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ export default function Footer() {
     return (
         <div id="contentFooter">
             <footer>
-                <p>This React.js project was made by <a href="https://github.com/wang-yi-yao">Wang Yi Yao</a> (Daniel Wang), 2021-2022<br/>
-                The source code of this webapp can be found in <a href="https://github.com/wang-yi-yao/xkcdWebapp">this repo</a><br/>
+                <p>This React.js project was made by <a href="https://github.com/frigadae">Yiyao Wang</a>, 2021-2022<br/>
+                The source code of this webapp can be found in <a href="https://github.com/frigadae/xkcdWebapp">this repo</a><br/>
                 More information on React.js can be found <a href="https://reactjs.org/">here</a><br/>
                 Comics are sourced from <a href="https://xkcd.com/">xkcd.com</a>. XKCD is by Randall Munroe <br/>
                 The XKCD API can be found <a href="https://xkcd.com/json.html">here</a></p>
